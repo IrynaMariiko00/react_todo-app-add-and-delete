fbe137419b9ed9985ca242f98f62cf4823329559
Iryna Mariiko <IrynaMM497@gmail.com>
2024-12-19 12:01:36 +0200

    copied code from previous task

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,78 @@
-/* eslint-disable max-len */
-/* eslint-disable jsx-a11y/control-has-associated-label */
-import React from 'react';
+import React, { useEffect, useState, useMemo } from 'react';
 import { UserWarning } from './UserWarning';
-
-const USER_ID = 0;
+import { Header } from './components/Header';
+import { Todo } from './types/Todo';
+import { getTodos, USER_ID } from './api/todos';
+import { TodoItem } from './components/TodoItem';
+import { Errors } from './components/Errors';
+import { Footer } from './components/Footer';
+import { filterTodos } from './utils/FilterTodo';
+import { FilterBy } from './types/FilterBy';
 
 export const App: React.FC = () => {
+  const [todos, setTodos] = useState<Todo[]>([]);
+  const [error, setError] = useState('');
+  const [currentFilter, setCurrentFilter] = useState<FilterBy>(FilterBy.All);
+
+  useEffect(() => {
+    setError('');
+
+    getTodos()
+      .then(todosFromApi => {
+        setTodos(todosFromApi);
+      })
+      .catch(() => {
+        setError('Unable to load todos');
+      });
+  }, []);
+
+  const filtered = useMemo(
+    () => filterTodos(todos, currentFilter),
+    [todos, currentFilter],
+  );
+
+  const handleNewTodo = (newTodo: Todo) => {
+    setTodos([...todos, newTodo]);
+  };
+
+  const toggleTodoStatus = (updatedTodo: Todo) => {
+    setTodos(prevTodos =>
+      prevTodos.map(todo => (todo.id === updatedTodo.id ? updatedTodo : todo)),
+    );
+  };
+
   if (!USER_ID) {
     return <UserWarning />;
   }
 
   return (
-    <section className="section container">
-      <p className="title is-4">
-        Copy all you need from the prev task:
-        <br />
-        <a href="https://github.com/mate-academy/react_todo-app-loading-todos#react-todo-app-load-todos">
-          React Todo App - Load Todos
-        </a>
-      </p>
+    <div className="todoapp">
+      <h1 className="todoapp__title">todos</h1>
 
-      <p className="subtitle">Styles are already copied</p>
-    </section>
+      <div className="todoapp__content">
+        <Header onTodo={handleNewTodo} onError={setError} />
+
+        <section className="todoapp__main" data-cy="TodoList">
+          {filtered.map(todo => (
+            <TodoItem
+              key={todo.id}
+              todo={todo}
+              onToggleStatus={toggleTodoStatus}
+            />
+          ))}
+        </section>
+
+        {todos.length !== 0 && (
+          <Footer
+            currentFilter={currentFilter}
+            setCurrentFilter={setCurrentFilter}
+            todos={todos}
+            setTodos={setTodos}
+          />
+        )}
+      </div>
+
+      <Errors error={error} onClearError={setError} />
+    </div>
   );
 };
